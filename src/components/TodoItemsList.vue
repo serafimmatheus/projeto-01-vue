b6f95717b6f617c0619a355c379bcc836fd8870e
Matheus Serafim <matheus18serafim@gmail.com>
2024-10-22 15:09:30 -0300

    Add axios dependency and update package-lock.json

---
+++ src/components/TodoItemsList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="space-y-2">
+    <TodoItems />
+  </div>
+</template>
+
+<script>
+import TodoItems from './TodoItems.vue'
+
+export default {
+  name: 'TodoSpinner',
+  components: {
+    TodoItems,
+  },
+}
+</script>
